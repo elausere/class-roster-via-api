41404aa81532ad950ca2b460368519c5dffdf5e0
elausere <eausere@ewu.edu>
2022-04-26 18:49:35 -0700

    Initial Styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,60 +1,31 @@
 // // My Scripts
 
 
-// // Data source
-// const url = "https://v1.nocodeapi.com/pmanikoth/webflow/wtzODovSIJsRXhZg";
-
-// // Get data
-// fetch(url)
-//   .then( response => response.json() )
-//   .then( data  => {
-//     // check-check
-//     console.log(data.items[0].image.url);
-    
-//     // Get Parent
-//     const container = document.querySelector('main');
-     
-//     // Loop through each item
-//     data.forEach( ( item ) => {
-      
-//      const template = `
-//      <figure>
-//         <img src=${student.image.url} />  
-//         <figcaption>${student}</figcaption>
-//      </figure>
-//      `
-//      // Dynamicaly insert into the DOM
-//       container.insertAdjacentHTML('afterbegin', template);
-
-//     });
-//   });
-
 // Data source
 const url = "https://v1.nocodeapi.com/pmanikoth/webflow/wtzODovSIJsRXhZg";
 
 // Get data
 fetch(url)
-  .then( response  => response.json())
+  .then( response => response.json() )
   .then( data  => {
-    // check-check: get one image
-    // Note: Webflow returns data in array called `items`
+    // check-check
     console.log(data.items[0].image.url);
-
-    // get container for data
-    const gallery = document.querySelector(".gallery");
-
-    //
-    data.items.forEach( student => {
+    
+    // Get Parent
+    const roster = document.querySelector('main');
+     
+    // Loop through each item
+    data.items.forEach( ( student ) => {
       
-      // template
-      const template = `
-          <figure>
-            <figcaption>Student</figcaption>
-            <img src="${student.image.url}" alt="Placeholder" />
-          </figure>
-       `;
+     const template = `
+     <figure>
+        <img src="${student.image.url}" alt="Student Image"  
+        <figcaption>${student.name}</figcaption>
+     </figure>
+     `;
+     // Dynamicaly insert into the DOM
+      roster.insertAdjacentHTML('afterbegin', template);
 
-      gallery.insertAdjacentHTML("afterbegin", template);
     });
   });
 
